59af49a0325b421a80d4539464b0898ca4f23440
beregmira <beregmira@yandex.ru>
2018-01-26 19:58:32 +0300

    + Add logical delete self comments or blog author ~ Change display editor page

--- src/main/java/ru/beregmira/SMSpring/controllers/BlogController.java
+++ src/main/java/ru/beregmira/SMSpring/controllers/BlogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.beregmira.SMSpring.model.Comment;
+import ru.beregmira.SMSpring.model.UserApp;
 import ru.beregmira.SMSpring.service.ArticleService;
 import ru.beregmira.SMSpring.service.CommentsService;
 import ru.beregmira.SMSpring.service.UserAppService;
@@ -31,10 +32,13 @@ public class BlogController {
     @RequestMapping(value = "/blog", params = {"id"}, method = GET)
     public String getSingleArticle(Model model, @AuthenticationPrincipal UserDetails currentUser,
                                    @RequestParam(value = "id") Long id) {
+        UserApp userApp = userAppService.getUserAppByName(currentUser.getUsername());
         model.addAttribute("SingleArticle", articleService.getSingleArticle(id));
         model.addAttribute("ListOfAllComments", commentsService.getAllCommentsByArticleId(id));
         model.addAttribute("NewComment", new Comment());
         model.addAttribute("CurrentUserAppId", userAppService.getUserAppIdByCurrentUserName(currentUser));
+        model.addAttribute("geInfoCommetns", userAppService);
+        model.addAttribute("currentUser", userApp);
         return "SingleArticle";
     }
 
@@ -42,5 +46,12 @@ public class BlogController {
     public String submitArticle(@ModelAttribute Comment NewComment) {
         commentsService.save(NewComment);
         return "redirect:../main";
+    }
+
+
+    @RequestMapping(value = "/blog/comment/delete", params = {"id"}, method = GET)
+    public String getDeleteSingleComments(@ModelAttribute Comment comment) {
+        commentsService.deleteCommentById(comment.getId());
+        return "redirect:/main";
     }
 }
